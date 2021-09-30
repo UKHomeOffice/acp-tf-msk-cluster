1f494a754d80a5be412702253775930943920df0
tasharnvb <tasharn.b@gmail.com>
2021-09-30 12:31:24 +0100

    Remove unneeded egress rule

--- main.tf
+++ main.tf
@@ -86,13 +86,6 @@ resource "aws_security_group" "sg_msk" {
     cidr_blocks = var.cidr_blocks
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(
     var.tags,
     {
