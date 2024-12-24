5a2ba9dcdb5b21beba2db9b2f5b81a1740344607
Babatunde Kassim <babatunde.kassim1@digital.homeoffice.gov.uk>
2024-12-24 11:42:00 +0000

    ACPENG-2357: add lifecycle
    Signed-off-by: Babatunde Kassim <babatunde.kassim1@digital.homeoffice.gov.uk>

--- main.tf
+++ main.tf
@@ -127,7 +127,8 @@ resource "aws_msk_cluster" "msk_kafka" {
     client_subnets  = var.subnet_ids
     security_groups = [aws_security_group.sg_msk.id]
   }
-
+  
+  # Adding lifecycle
   lifecycle {
     ignore_changes = [
       client_authentication["sasl"],
